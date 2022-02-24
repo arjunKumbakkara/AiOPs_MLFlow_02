09bbf9abb023818cf92d8aff0dcd78fdd78ed0f6
arjunkumbakkara <arjunkumbakkara@gmail.com>
2022-02-24 21:35:47 +0530

    parameters added with runs

--- runs.py
+++ runs.py
@@ -1,14 +1,16 @@
 import os
+import numpy as np
+from pyparsing import alphas
 
 
 
 
-p1s = range(0,5)
-p2s = range(0,5)
+alphas = np.linspace(0.1,1.0,5)
+l1_ratios = np.linspace(0.1,1.0,5)
 
 #This is a driver
 
-for p1 in p1s:
-    for p2 in p2s:
-        print(f"logging experiment for p1 {p1} and p2 {p2}")
-        os.system(f"python demo.py -p1 {p1}   -p2 {p2}")
+for alpha in alphas:
+    for l1 in l1_ratios:
+        print(f"logging experiment for alpha: {alpha} and l1_ratio {l1}")
+        os.system(f"python demo.py -a {alpha}   -l1 {l1}")
